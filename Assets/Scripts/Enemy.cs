3c5ab949f9f957a755932bd5857bcaa4ec6199c6
michal nawrot <michal.nawrot.mn.mn@gmail.com>
2023-10-20 03:13:00 +0200

    add virtua cop style circles around enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,11 @@ public class Enemy : MonoBehaviour
     private GameObject explosion;
     private float duration;
     private float time = 0;
-    private const int TIME_TO_ATTACK = 10;
+    private const int TIME_TO_ATTACK = 8;
+    private GameObject aimlock;
+    private GameObject aimCircle1;
+    private GameObject aimCircle2;
+    private Material aimlockMaterial;
 
     private bool start = false;
     [SerializeField]
@@ -24,22 +28,32 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
+        transform.LookAt(Camera.main.transform);
         explosion = this.gameObject.transform.GetChild(0).gameObject;
+        aimlock = this.gameObject.transform.GetChild(1).gameObject;
+        aimCircle1 = this.gameObject.transform.GetChild(2).gameObject;
+        aimCircle2 = this.gameObject.transform.GetChild(3).gameObject;
+
+        aimlockMaterial = aimlock.GetComponent<MeshRenderer>().material;
         explosion.SetActive(false);
         duration = explosion.GetComponent<ParticleSystem>().main.duration - 1;
         explosion.GetComponent<ParticleSystem>().Stop();
         StartCoroutine(CountdownToAttack());
         StartCoroutine(Shaking());
+        StartCoroutine(AimlockController());
     }
 
     private void Attack()
     {
         gameObject.layer = 0;
+        Destroy(aimlock);
+        StopCoroutine(AimlockController());
         explosion.SetActive(true);
         Camera.main.GetComponentInChildren<Player>().GetHit();
         explosion.GetComponent<ParticleSystem>().Play();
         gameObject.GetComponent<MeshRenderer>().enabled = false;
         StartCoroutine(CountdownToExtinction());
+
     }
 
     private IEnumerator CountdownToExtinction()
@@ -69,14 +83,64 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private IEnumerator AimlockController()
+    {
+        Debug.Log("Aimlock");
+        Vector3 scaleChange = new Vector3(0.003f, 0.003f, 0.003f);
+
+        bool wasYellow = false;
+
+        while (time <= TIME_TO_ATTACK)
+        {
+            if (aimlock.transform.localScale.x >= 10)
+            {
+                aimlock.transform.localScale -= scaleChange * 300;
+            }
+
+            if (aimCircle1 != null)
+            {
+                aimCircle1.transform.localScale -= (scaleChange * 500);
+                if (aimCircle1.transform.localScale.x <= 0)
+                {
+                    Destroy(aimCircle1);
+                }
+            }
+
+            if (aimCircle2 != null)
+            {
+                aimCircle2.transform.localScale -= (scaleChange * 900);
+                if (aimCircle2.transform.localScale.x <= 0)
+                {
+                    Destroy(aimCircle2);
+                }
+            }
+
+            aimlock.transform.Rotate(0f, 75 * Time.fixedDeltaTime, 0f);
+
+            if (wasYellow)
+            {
+                aimlockMaterial.color = new Color(aimlockMaterial.color.r, aimlockMaterial.color.g - Time.fixedDeltaTime * 0.4f, aimlockMaterial.color.b);
+            }
+            else
+            {
+                aimlockMaterial.color = new Color(aimlockMaterial.color.r + Time.fixedDeltaTime * 0.35f, aimlockMaterial.color.g, aimlockMaterial.color.b);
+                if (aimlockMaterial.color.r >= 1 && aimlockMaterial.color.g >= 1)
+                {
+                    wasYellow = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
     private IEnumerator Shaking()
     {
-        Debug.Log("Shaking");
+
         Vector3 startPostition = transform.position;
 
         while (time <= TIME_TO_ATTACK)
         {
-            float strength = curve.Evaluate(time / 10);
+            float strength = curve.Evaluate(time / TIME_TO_ATTACK);
             transform.position = startPostition + Random.insideUnitSphere * strength;
             yield return null;
         }
