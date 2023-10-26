8e1df1bf8890afe751774c39926ffeb174818208
michal nawrot <michal.nawrot.mn.mn@gmail.com>
2023-10-27 01:28:54 +0200

    zooming and moving change

--- Assets/Scripts/Zooming.cs
+++ Assets/Scripts/Zooming.cs
@@ -25,6 +25,7 @@ public class Zooming : MonoBehaviour
 
     public IEnumerator ZoomOnEnemy()
     {
+        minFieldOfView = 35.0f;
         if (enemy.GetComponent<Enemy>()) { 
             time = enemy.GetComponent<Enemy>()._Time;
             time_to_attack = enemy.GetComponent<Enemy>()._TIME_TO_ATTACK;
@@ -35,9 +36,26 @@ public class Zooming : MonoBehaviour
             time_to_attack = enemy.GetComponent<EnemyHard>()._TIME_TO_ATTACK;
         }
 
+        Vector3 dir = enemy.transform.position - Camera.main.transform.position;
+        Ray ray = new Ray(Camera.main.transform.position, dir);
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 10000))
+        {
+            Debug.Log("distance: " + hit.distance);
+            Debug.Log("Log(distance): " + Mathf.Log(hit.distance*1.7f, 1.2f));
+            minFieldOfView = minFieldOfView - Mathf.Log(hit.distance*1.7f, 1.2f);
+            if(minFieldOfView < 6)
+            {
+                minFieldOfView = 6;
+            }
+            
+        }
+        Debug.Log("minField: " + minFieldOfView);
+
         while (time <= time_to_attack)
         {
-            zoom -= 10 * zoomMultiplier;
+            zoom -= 5 * zoomMultiplier;
             zoom = Mathf.Clamp(zoom, minFieldOfView, maxFieldOfView);
 
             cam.fieldOfView = Mathf.SmoothDamp(cam.fieldOfView, zoom, ref velocity, smoothTime);
@@ -82,7 +100,7 @@ public class Zooming : MonoBehaviour
         while (time <= time_to_attack)
         {
             Quaternion targetRotation = Quaternion.LookRotation(targetGameObject.transform.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.8f * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1.5f * Time.deltaTime);
             yield return null;
         }
     }
@@ -100,7 +118,7 @@ public class Zooming : MonoBehaviour
             //check if zoomed out -> stop coroutine of zoomingOut then change transform.LookAt to default 
             Quaternion targetRotation = Quaternion.LookRotation(gameobjectToWatch.transform.position - transform.position);
             if(transform.rotation != targetRotation && !rotated) { 
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5 * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1.5f * Time.deltaTime);
             }
             else
             {
